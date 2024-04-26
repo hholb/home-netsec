4eff17082643d4e4ebd13de65e14c89b9571bbf8
hholb <hholbroo@unca.edu>
2024-04-26 10:36:56 -0400

    Add better error handling

--- pihole-setup.sh
+++ pihole-setup.sh
@@ -1,20 +1,30 @@
+#!/usr/bin/env bash
+
 set -e
 
-git clone --depth 1 https://github.com/pi-hole/pi-hole.git
+# clone the pi-hole repo if it isn't already here
+if ! [[ -d "./pi-hole" ]]; then
+    git clone --depth 1 https://github.com/pi-hole/pi-hole.git
+fi
 
+# run the pi-hole install script
 if [[ -d "./pi-hole/automated install/" ]]; then
-   pushd "pi-hole/automated install/"
-   bash ./basic-install.sh
-   popd
+    pushd "pi-hole/automated install/"
+    PIHOLE_SKIP_OS_CHECK=true bash ./basic-install.sh
+    popd
 else
     echo "Failed to install pihole."
     exit 1
 fi
 
+# update the adlists in the database
 if [[ -f "./pihole-adlist.txt" ]]; then
     echo "Updating adlist..."
-    python3 update-adlist
+    python3 update-adlist.py
 else
     echo "Unable to update adlist."
     exit 1
 fi
+
+# tell pihole to pull the domains from the new adlists
+pihole -g
--- update-adlist.py
+++ update-adlist.py
@@ -2,34 +2,47 @@
 
 import sqlite3
 
-def parse_adlist(filename):
-    f = open(filename)
-    domains = []
-    while True:
-        r = f.readline()
-        if r == "" or r == "\n":
-            break
-        else:
-            domains.append(r.removesuffix("\n"))
-    return(domains)
+def parse_adlist(filename: str)-> list[str]:
+    with open(filename) as f:
+        domains = [d for d in f]
+
+    return (d.removesuffix("\n") for d in domains)
 
 
-def insert_adlist(domains):
-    domains = ({"address": a} for a in domains)
+def insert_adlist(domains: list[str])->int:
+    num_inserts = 0
     with sqlite3.connect("/etc/pihole/gravity.db") as con:
         try:
-            cursor = con.cursor()
-            q = "INSERT OR IGNORE INTO adlist (address) VALUES (:address)"
-            cursor.executemany(q, domains)
+            for d in domains:
+                cursor = con.cursor()
+                q = "INSERT OR IGNORE INTO adlist (address) VALUES (:address)"
+                domain = {"address": d}
+                cursor.execute(q, domain)
+                num_inserts += 1
         except Exception as e:
-            print(e)
-            exit(1)
+            raise e
+    return num_inserts
 
 
 def main():
     filename = "pihole-adlist.txt"
-    domains = parse_adlist(filename)
-    insert_adlist(domains)
+
+    try:
+        domains = parse_adlist(filename)
+    except FileNotFoundError as e:
+        print(e)
+        print(f"Could not open {filename}")
+        print("Try cloning the repo again or running as root")
+        exit(1)
+
+    try:
+        inserts = insert_adlist(domains)
+        print(f"Inserted {inserts} domains.")
+    except sqlite3.OperationalError as e:
+        print(e)
+        print("Could not write to pihole database")
+        print("Try running as root")
+        exit(1)
 
 
 if __name__ == "__main__":
